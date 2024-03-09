d86d8ea782c43dab5aace6b779d43b967f21a5db
samiya-k <samiya@hicounselor.com>
2024-03-09 13:39:33 +0530

    First Commit

---
+++ src/main/java/com/samiya/learnjava/Ch1_Introduction_to_Java/A_FirstProgram.java
@@ -0,0 +1,17 @@
+package main.java.com.samiya.learnjava.Ch1_Introduction_to_Java;
+
+/*Java is a popular programming language, created in 1995. It is owned by Oracle.
+It is used for:
+    Mobile applications (specially Android apps)
+    Desktop applications
+    Web applications
+    Web servers and application servers
+    Games
+    Database connection
+    And much, much more!
+ */
+public class A_FirstProgram {
+    public static void main(String[] args){
+        System.out.println("Hello everyone!");
+    }
+}
---
+++ src/main/java/com/samiya/learnjava/Ch1_Introduction_to_Java/B_Print.java
@@ -0,0 +1,20 @@
+package main.java.com.samiya.learnjava.Ch1_Introduction_to_Java;
+
+public class B_Print{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        
+        // print() method does not insert a new line at the end of the output.
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+
+        // print numbers
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    }
+}
