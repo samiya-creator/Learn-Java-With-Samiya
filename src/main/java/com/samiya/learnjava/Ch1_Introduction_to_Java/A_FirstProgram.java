cc5853d574b8e8937371bd38460756529e9fdae8
samiya-k <samiya@hicounselor.com>
2024-03-09 13:51:25 +0530

    Chanpter 1 and Chapter 2

--- src/main/java/com/samiya/learnjava/Ch1_Introduction_to_Java/A_FirstProgram.java
+++ src/main/java/com/samiya/learnjava/Ch1_Introduction_to_Java/A_FirstProgram.java
@@ -1,14 +1,15 @@
 package main.java.com.samiya.learnjava.Ch1_Introduction_to_Java;
 
-/*Java is a popular programming language, created in 1995. It is owned by Oracle.
-It is used for:
-    Mobile applications (specially Android apps)
-    Desktop applications
-    Web applications
-    Web servers and application servers
-    Games
-    Database connection
-    And much, much more!
+/*
+ * Java is a popular programming language, created in 1995. It is owned by Oracle.
+ * It is used for:
+ *    - Mobile applications (especially Android apps)
+ *    - Desktop applications
+ *    - Web applications
+ *    - Web servers and application servers
+ *    - Games
+ *    - Database connection
+ *    - And much, much more!
  */
 public class A_FirstProgram {
     public static void main(String[] args){
--- src/main/java/com/samiya/learnjava/Ch1_Introduction_to_Java/B_Print.java
+++ src/main/java/com/samiya/learnjava/Ch1_Introduction_to_Java/B_Print.java
@@ -1,20 +1,24 @@
 package main.java.com.samiya.learnjava.Ch1_Introduction_to_Java;
 
-public class B_Print{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("I am learning Java.");
-        System.out.println("It is awesome!");
-        
-        // print() method does not insert a new line at the end of the output.
-        System.out.print("Hello World! ");
-        System.out.print("I will print on the same line.");
+public class B_Print {
+    public static void main(String[] args) {
+        // Print statements to display messages
+        System.out.println("Welcome to Java Programming!");
+        System.out.println("I'm excited to explore Java's features.");
+        System.out.println("Let's begin our journey!");
 
-        // print numbers
+        // The print() method does not insert a new line at the end of the output
+        System.out.print("Hello World! ");
+        System.out.print("This is a demonstration of the print() method, ");
+        System.out.print("which prints text on the same line.");
+
+        // Print numbers and expressions
         System.out.println(3);
         System.out.println(358);
         System.out.println(50000);
-        System.out.println(3 + 3);
-        System.out.println(2 * 5);
+
+        // Print the result of mathematical expressions
+        System.out.println("3 + 3 = " + (3 + 3)); // Addition
+        System.out.println("2 * 5 = " + (2 * 5)); // Multiplication
     }
 }
